5831f8ac7befb3159d9e21305003e4e0ce585d93
FS-Sridharan007 <sridharans592@gmail.com>
2026-03-23 19:27:10 +0530

    added code files

---
+++ code/data_prep.py
@@ -0,0 +1,9 @@
+# Name: SRIDHARAN.S | Roll No: 51
+
+import datetime
+
+print("Roll No: 51")
+print("Timestamp:", datetime.datetime.now())
+
+# Dummy data prep step
+print("Data preparation completed")
---
+++ code/evaluate.py
@@ -0,0 +1,9 @@
+# Name: SRIDHARAN.S | Roll No: 51
+
+import datetime
+
+print("Roll No: 51")
+print("Timestamp:", datetime.datetime.now())
+
+# Dummy evaluation step
+print("Evaluation completed")
